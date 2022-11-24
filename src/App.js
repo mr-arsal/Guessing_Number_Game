ece5dc4e7c235169ce1b80313981579c32f0fbd3
Mr-Arsal <an24547@gmail.com>
2022-11-24 23:38:51 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const game = () => {
+    let random = Math.floor(Math.random() * 101);
+    console.log(random)
+    let input;
+    do {
+      input = prompt('Guess any number between 0 and 100');
+      input = Number.parseInt(input)
+      if (input < random) {
+        alert('Guessed Number is less than correct number')
+      } else if (input > random) {
+        alert('Guessed Number is higher than correct number')
+      } else if (input === random) {
+        alert(`Congartulations! You win the game. ${input} is the correct number.`)
+      }
+    } while (input !== random)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background'>
+      <h1 className='fs-4'>Guess the correct number 
+        <br />
+        &#38; 
+        <br />
+        Win the game</h1>
+      <button onClick={game}>
+        Start the game
+      </button>
+      <div className='caution'>
+        <strong className=''>Warning: </strong>
+        <br />
+        You can't go back unless 
+        <br />
+        you guess the correct number. 
+      </div>
     </div>
+
   );
 }
 
