45dd74a778688ca092f803f1224b4032be547387
Mr-Arsal <an24547@gmail.com>
2022-11-25 19:02:55 +0500

    Landing page text updated

--- src/App.js
+++ src/App.js
@@ -7,25 +7,32 @@ function App() {
     let random = Math.floor(Math.random() * 101);
     console.log(random)
     let input;
+    let attempts = 10;
     do {
-      input = prompt('Guess any number between 0 and 100');
-      input = Number.parseInt(input)
-      if (input < random) {
-        alert('Guessed Number is less than correct number')
-      } else if (input > random) {
-        alert('Guessed Number is higher than correct number')
+      input = prompt(`Guess any number between 0 and 100.
+      ${attempts} attempts have left.`
+      );
+      input = Number.parseInt(input);
+      attempts = attempts - 1;
+      if (input < random && attempts !== 0) {
+        alert('Guessed Number Is Lower Than Correct Number')
+      } else if (input > random && attempts !== 0) {
+        alert('Guessed Number Is Higher Than Correct Number')
+      } else if (attempts === 0 && input !== random) {
+        alert('Try Again❗')
       } else if (input === random) {
-        alert(`Congartulations! You win the game. ${input} is the correct number.`)
+        alert(`Congartulations🎉 You won the game in ${10 - attempts} attempts. 
+        ${input} is the correct number.`)
       }
-    } while (input !== random)
+    } while (input !== random && attempts !== 0)
   }
 
 
   return (
     <div className='background'>
-      <h1 className='fs-4'>Guess the correct number 
+      <h1 className='fs-4'>Guess the secret number
         <br />
-        &#38; 
+        &#38;
         <br />
         Win the game</h1>
       <button onClick={game}>
@@ -34,10 +41,9 @@ function App() {
       <div className='caution'>
         <strong className=''>Warning: </strong>
         <br />
-        You can't go back unless 
+        If you try to cancel the game
         <br />
-        you guess the correct number. 
-      </div>
+        It will cost you 1 attempt!</div>
     </div>
 
   );
